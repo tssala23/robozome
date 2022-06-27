ac386f1101840e73e2fb6f466872ee4345bef038
tssala23 <71083414+tssala23@users.noreply.github.com>
2022-06-27 11:02:22 -0400

    App will respond to on-boarding issue form only

--- TestApp.js
+++ TestApp.js
@@ -12,20 +12,21 @@ module.exports = (app) => {
     });
 
     const body = context.payload.issue["body"];
-
     const metaData = body.split("\n\n");
 
-    const cluster = metaData[0] + ": " + metaData[1];
-    const teamName = metaData[2] + ": " + metaData[3];
-    const namespace = metaData[4] + ": " + metaData[5];
-    const description = metaData[6] + ": " + metaData[7];
-    const userAccess = metaData[8] + ": " + metaData[9];
-    const quota = metaData[10] + ": " + metaData[11];
-    const customQuota = metaData[12] + ": " + metaData[13];
-    const GPG = metaData[14] + ": " + metaData[15];
+    if(metaData[0] == "### Target cluster"){
+      const cluster = metaData[0] + ": " + metaData[1];
+      const teamName = metaData[2] + ": " + metaData[3];
+      const namespace = metaData[4] + ": " + metaData[5];
+      const description = metaData[6] + ": " + metaData[7];
+      const userAccess = metaData[8] + ": " + metaData[9];
+      const quota = metaData[10] + ": " + metaData[11];
+      const customQuota = metaData[12] + ": " + metaData[13];
+      const GPG = metaData[14] + ": " + metaData[15];
 
+      return context.octokit.issues.createComment(issueComment);
+    }
 
-    return context.octokit.issues.createComment(issueComment);
   });
 
 };
