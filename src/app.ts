e44e2b5afaa01192cc77d73a103bdcbe6dcbcfcb
Taj Salawu <tsalawu@redhat.com>
2022-07-08 14:26:06 -0400

    feat: Added serialization of data to JSON
    Signed-off-by: Taj Salawu <tsalawu@redhat.com>

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import {
   updateTokenSecret,
   useApi,
 } from '@operate-first/probot-kubernetes';
+import { parse } from '@open-service-group/probot-issue-form';
 
 const generateTaskPayload = (name: string, context: any) => ({
   apiVersion: 'tekton.dev/v1beta1',
@@ -74,6 +75,48 @@ export default (
     help: 'Metrics for action triggered by the operator with respect to the kubernetes operations.',
     labelNames: ['install', 'operation', 'status', 'method'],
   });
+
+  //From peribolos app.ts
+  const createTaskRun = (
+    name: string,
+    context: any,
+    extraParams: Array<Record<string, unknown>> = []
+  ) => {
+    const params = [
+      {
+        name: 'SECRET_NAME',
+        value: getTokenSecretName(context),
+      },
+      ...extraParams,
+    ];
+    const taskRunpayload = {
+      apiVersion: 'tekton.dev/v1beta1',
+      kind: 'TaskRun',
+      metadata: {
+        generateName: name + '-',
+      },
+      spec: {
+        taskRef: {
+          name,
+        },
+        params: params,
+      },
+    };
+
+    wrapOperationWithMetrics(
+      useApi(APIS.CustomObjectsApi).createNamespacedCustomObject(
+        'tekton.dev',
+        'v1beta1',
+        getNamespace(),
+        'taskruns',
+        taskRunpayload
+      ),
+      {
+        install: context.payload.installation.id,
+        method: name,
+      }
+    );
+  };
 
   // Simple callback wrapper - executes and async operation and based on the result it inc() operationsTriggered counted
   const wrapOperationWithMetrics = (callback: Promise<any>, labels: any) => {
@@ -145,5 +188,58 @@ export default (
       install: context.payload.installation.id,
       method: 'deleteSecret',
     });
+  });
+
+  app.on("issues", async(context: any) => {
+    const issueComment = context.issue({
+           body: "Thanks for submitting onboarding request!",
+         });
+    
+    // try {
+    //     const data = await parse(context);
+    // } catch {
+    //     app.log.info('Issue was not created using Issue form template (the YAML ones)');
+    //     console.log("We in here but failed");
+    // }
+    
+    const data = { //test data until parse working
+      targetCluster: ["cluster"],
+      teamName: "name",
+      desiredProjectName: "namespace",
+      projectDescription: "description of project",
+      usersNeedingAccess: "users",
+      namespaceQuota: ["custom"],
+      customQuota: "custTest",
+      GPGKey: ""
+    };
+
+    var cluster:string;
+    if (data["targetCluster"][0] == "smaug" || data["targetCluster"][0] == "infra"){
+      cluster = "moc/" + data["targetCluster"];
+    } else{
+      cluster = "emea/" + data["targetCluster"];
+    };
+
+    var quota:string;
+    if (data["namespaceQuota"][0] == "custom"){
+      quota = data["customQuota"];
+    } else{
+      quota = data["namespaceQuota"][0];
+    }
+
+    const payloadDict = {
+      namespace: data["desiredProjectName"],
+      group: data["teamName"],
+      quota: quota,
+      cluster: cluster,
+      description: data["projectDescription"],
+      users: data["usersNeedingAccess"],
+      GPG: data["GPGKey"]
+    };
+
+    const payload = JSON.stringify(payloadDict);
+
+    createTaskRun('robozome', context)
+
   });
 };
